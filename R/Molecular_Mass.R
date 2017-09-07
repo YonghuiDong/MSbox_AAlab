363f180820c9f58886a970e499c0761581a10f40
YonghuiDong <Dong@users-MacBook-Pro.local>
2017-09-07 23:15:09 +0300

    Debug: case insensitive; change decimal to 6

--- R/Molecular_Mass.R
+++ R/Molecular_Mass.R
@@ -7,23 +7,27 @@
 #'  M_mass('C7H6O4')
 #'  M_mass('c7H6O4') # case insensitive
 
-# calculate accurate molecular mass
 M_mass <- function(F) {
-  ## read element data, and find the element with the highest abundance
+  #(1) read element data, and find the element with the highest abundance
   element <- as.data.frame(sysdata$element)
   element$Abund.<- as.numeric(element$Abund.)
   element.agg <- aggregate(Abund. ~ Class, element, max)
   element.max <- merge(element.agg, element)
-  ## split the mass formula
+  element.max$Class <- toupper(element.max$Class)
+  #(2) split the mass formula
   v1 <- strsplit(F, "(?<=[A-Za-z])(?=[0-9])|(?<=[0-9])(?=[A-Za-z])", perl = TRUE)[[1]]
   atom <- v1[c(TRUE, FALSE)]
-  # convert the first letter of atom to upper case. case insensitive.
-  atom <- paste(toupper(substr(atom, 1, 1)), substr(atom, 2, nchar(atom)), sep="")
+  ## convert the first letter of atom to upper case. case insensitive.
+  atom <- toupper(atom)
+  ## check the input
+  atom_match <- match(atom, element.max$Class)
+  if(any(is.na(atom_match))) {stop("Warning: certain element not found")}
   num <- as.numeric(v1[c(FALSE, TRUE)])
   if (length(atom) == length(num)) {
-    options(digits=12)
     atom_mass <- element.max$Mass[match(atom, element.max$Class)]
     accurate_mass <- sum(atom_mass*num)
+    ## set 6
+    accurate_mass = formatC(accurate_mass, digits = 6, format = "f")
     return(accurate_mass)
   }
   else
