816655ed61fb8d614c1be8c657a733bc48e2c396
YonghuiDong <Dong@users-MacBook-Pro.local>
2018-07-22 00:26:50 +0300

    Add formula function

---
+++ R/get_formula.R
@@ -0,0 +1,19 @@
+#' @title Get compound formula
+#' @description get compound formula from https://cactus.nci.nih.gov/chemical/structure
+#' @param chem, chemical name of the compound
+#' @param representation, representation methods, formula is default
+#' @import xml2
+#' @importFrom utils URLencode
+#' @export
+#' @examples
+#' formula('malic acid')
+
+formula <- function(chem, representation = 'formula') {
+  ref_url <- "https://cactus.nci.nih.gov/chemical/structure"
+  chem_url <- paste(ref_url, URLencode(chem), representation, 'xml', sep = '/')
+  ## restrict query time
+  Sys.sleep(1.1)
+  myread <- read_xml(chem_url)
+  myresult <- xml_text(xml_find_all(myread, '//item'))[[1]]
+  return(myresult)
+}
