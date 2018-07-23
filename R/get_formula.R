90b047b9d954cc412c3e13cbcca806b1927e52d8
YonghuiDong <Dong@users-MacBook-Pro.local>
2018-07-23 12:00:54 +0300

    Change the message format

--- R/get_formula.R
+++ R/get_formula.R
@@ -15,9 +15,25 @@ formula <- function(chem, representation = 'formula', info = FALSE) {
 
   ##(1): display representation parameters
   if(info == TRUE) {message('More molecular information can be obtained by setting the representation parameter to:
-mw, monoisotopic_mass, h_bond_donor_count, h_bond_acceptor_count, h_bond_center_count, rule_of_5_violation_count,
-rotor_count, effective_rotor_count, ring_count, ringsys_count, xlogp2, heteroatom_count, hydrogen_atom_count,
-heavy_atom_count,deprotonable_group_count, protonable_group_count')}
+
+(1) mw: molecular weight,
+(2) monoisotopic_mass: monoisotopic_mass,
+(3) h_bond_donor_count: number of hydrogen bond donors,
+(4) h_bond_acceptor_count: number of hydrogen bond acceptors,
+(5) h_bond_center_count: number of heavy atoms acting as hydrogen bond donor or acceptor,
+(6) rule_of_5_violation_count: rule of 5 violation count,
+(7) rotor_count,
+(8) effective_rotor_count: Effective Rotor Count
+(9) ring_count,
+(10) ringsys_count,
+(11) xlogp2,
+(12) heteroatom_count,
+(13) hydrogen_atom_count,
+(14) heavy_atom_count,
+(15) deprotonable_group_count,
+(16) protonable_group_count
+
+--------------------------------------------------------------------------------------------')}
 
   ##(2): query compound information
   root <- "https://cactus.nci.nih.gov/chemical/structure"
