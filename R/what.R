91484668236b39d3ef29e46fe80a9719cdd9edff
YonghuiDong <yonghui.dong@gmail.com>
2019-02-23 09:12:33 +0200

    new what() function

--- R/what.R
+++ R/what.R
@@ -1,48 +1,60 @@
-#' @title search for m/z in home database
-#' @description tentative metabolite identification based on m/z value search in a home database
+#' @title search for m/z in from the idiom metabolomics database
+#' @description tentative metabolite identification based on m/z value search
 #' @author Yonghui Dong
 #' @param mz  m/z value
 #' @param ppm mass tolerance, default value = 10
 #' @param mode ionization mode, either positive '+' or negative '-'
-#' @param relatives, shuould the corresponding adducts, fragments and parent ions shown
 #' @export
 #' @examples
 #'  what(133.014, ppm = 10, mode = '-')
-#'  what(133.014, ppm = 10, mode = '-', relative = TRUE)
 
-what <- function (mz, mode = c('+', '-'), ppm = 10, relatives = FALSE) {
+what <- function (mz, mode = c('+', '-'), ppm = 10) {
 
   ##(1) input check
-  if(is.numeric(mz) == FALSE) {stop("Warning: mass to charge ratio mz shoule be numeric!")}
-  if(mode != "+" & mode !="-") {stop("WARNING: ion mode invalid. Choose '+' or '-'.\n")}
+  if(is.numeric(mz) == FALSE) {stop("warning: mass to charge ratio mz shoule be numeric!")}
+  if(mode != "+" & mode !="-") {stop("warning: ion mode invalid. Choose '+' or '-'.\n")}
 
   ##(2) load database
-  DB_pos <- as.data.frame(sysdata$analyticon_pos)
-  DB_neg <- as.data.frame(sysdata$analyticon_neg)
+  DB <- as.data.frame(sysdata$plant_db)
 
   ##(3) search in database
-  expand.grid.df <- function(...) Reduce(function(...) merge(..., by=NULL),
-                                         list(...))
+  expand.grid.df <- function(...) Reduce(function(...) merge(..., by = NULL), list(...))
+  Result <- vector("list", length(mz))
+
   if(mode == '-') {
-    DB.list <- expand.grid.df(mz, DB_neg)
-    colnames(DB.list)[1] <- 'my_mz'
-    myppm <- with(DB.list, abs(my_mz - mz) * 10^6 / mz)
-    Result = DB.list[(myppm <= ppm), ]
+    for (i in 1:length(mz)) {
+      DB.list <- expand.grid.df(mz[i], DB[, -(3:5)])
+      colnames(DB.list)[1] <- "search"
+      cal_ppm <- with(DB.list, (DB.list$`[M-H]` - search) * 10^6 / DB.list$`[M-H]`)
+      cal_ppm <-  round(cal_ppm, digits = 2)
+      DB.list <- cbind(DB.list, ppm = cal_ppm)
+      Result[[i]] = DB.list[(abs(cal_ppm) <= ppm), ]
+    }
   } else {
-    DB.list <- expand.grid.df(mz, DB_pos)
-    colnames(DB.list)[1] <- 'my_mz'
-    myppm <- with(DB.list, abs(my_mz - mz) * 10^6 / mz)
-    Result = DB.list[(myppm <= ppm), ]
+    for (i in 1:length(mz)) {
+      ## get [M+H]
+      DB.list_H <- expand.grid.df(mz[i], "[M+H]+", DB[, -(4:6)])
+      colnames(DB.list_H)[c(1, 2, 5)] <- c("search", "Adduct", "mzs")
+      ## get [M+Na]
+      DB.list_Na <- expand.grid.df(mz[i], "[M+Na]+", DB[, -c(3, 5, 6)])
+      colnames(DB.list_Na)[c(1, 2, 5)] <- c("search", "Adduct", "mzs")
+      ## get [M+K]
+      DB.list_K <- expand.grid.df(mz[i], "[M+K]+", DB[, -c(3, 4, 6)])
+      colnames(DB.list_K)[c(1, 2, 5)] <- c("search", "Adduct", "mzs")
+      ## combine them
+      DB.list <- rbind(DB.list_H, DB.list_Na, DB.list_K)
+      cal_ppm <- with(DB.list, (mzs - search) * 10^6 / mzs)
+      cal_ppm <-  round(cal_ppm, digits = 2)
+      DB.list <- cbind(DB.list, ppm = cal_ppm)
+      Result[[i]] = DB.list[(abs(cal_ppm) <= ppm), ]
+    }
   }
+  search_result <- do.call(rbind.data.frame, Result)
 
-  ##(4) check if Result is empty
-  if(nrow(Result) == 0) stop('Not Found, Unknown')
-
-  ##(5) return result
-  if(nrow(Result) != 0) {
-   ## include rows which have the same ID as the selected m/z (same parent ion).
-    search_result <- DB.list[DB.list$ID %in% Result$ID,]
-  } else
-    message('Not Found, unknown')
-  ifelse(relatives == TRUE, return(search_result), return(Result))
+  ##(4) check if Result is empty, and return result
+  if(nrow(search_result) == 0) {
+    message('Not Found, Unknown')
+  } else {
+    return(search_result)
+  }
 }
