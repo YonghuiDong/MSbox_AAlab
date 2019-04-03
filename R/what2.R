4e584bb1556f7b3c1fa507dc9f02bfd492359189
YonghuiDong <yonghui.dong@gmail.com>
2019-04-03 22:04:43 +0300

    update deisotoping using JJ's script
    Attention: this script needs original data

--- R/what2.R
+++ R/what2.R
@@ -73,9 +73,7 @@ what2 <- function (xset, RT.DB = NULL, use.DB = "METABOLOMICS", mode = "+", RT.c
   colnames(peak)[-c(1:(7 + length(pheno_levels)))] <- paste(my_meta$class, "_", row.names(my_meta), sep = "")
 
   ##(3.3) deisotoping
-  an <- xsAnnotate(xset)
-  anG <- groupFWHM(an)
-  anI <- findIsotopes(an)
+  anI = groupCorr(xset, calcIso = TRUE, calcCiS = TRUE, cor_eic_th = 0.9)
   iso_peaklist <- getPeaklist(anI)
   iso_peaklist$isotopes <- sub("\\[.*?\\]", "", iso_peaklist$isotopes)
   peak <- peak[iso_peaklist$isotopes == '' | iso_peaklist$isotopes == '[M]+', ]
